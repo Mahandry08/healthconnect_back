0a8f9a3f2676613681e1b3135a63ad586412a1d2
Mahandry Razakavonison <80761003+Mahandry08@users.noreply.github.com>
2025-02-21 11:07:22 +0300

    new qa configs updates

--- app.ts
+++ app.ts
@@ -1,45 +1,3 @@
-/* FOR MONGO CODE */
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import bodyParser from 'body-parser';
-// import dotenv from 'dotenv'; 
-// import consultationRoutes from './src/routes/consultationRoutes';
-// import prescriptionRoutes from './src/routes/prescriptionRoutes';
-// import userRoutes from './src/routes/userRoutes';
-
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// const mongoURI = process.env.MONGODB_URI; // Use the environment variable
-
-// if (!mongoURI) {
-//     console.error('MongoDB URI is not defined in the .env file.');
-//     process.exit(1); 
-// }
-
-// mongoose.connect(mongoURI)
-//     .then(() => console.log('Database connected !'))
-//     .catch(err => console.error('MongoDB connection error:', err));
-
-// app.use('/api/consultations', consultationRoutes);
-// app.use('/api/prescriptions', prescriptionRoutes);
-// app.use('/api/users', userRoutes);
-
-// app.get('/', (req, res) => {
-//     res.send('Welcome to the HealthConnect API!');
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
@@ -51,8 +9,10 @@ import userRoutes from './src/routes/userRoutes';
 dotenv.config();
 
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -71,5 +31,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://192.168.88.8:${PORT}`);
+    //console.log(`Server is running on http://192.168.88.8:${PORT}`); //for physical device
+    console.log(`Server is running on http://localhost:${PORT}`); //for android emulator
 });
